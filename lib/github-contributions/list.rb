f72a345f76c894659db5a3d17dc7e817c071dddb
Hemali Jain <Hemalijains@gmail.com>
2014-02-19 06:48:57 -0800

    to ouput only repo

--- lib/github-contributions/list.rb
+++ lib/github-contributions/list.rb
@@ -6,8 +6,12 @@ module Github
       attr_reader :connection,:current_user
       def initialize connection,current_user, options = {}
         @current_user=current_user
-        @connection = connection
-        find_list_of_orgs
+        @connection = connection 
+      end
+
+      def list
+        @list=find_list_of_orgs
+        @list.flatten.compact.uniq
       end
 
       def find_list_of_orgs
@@ -15,19 +19,23 @@ module Github
         @repos_contributions=Array.new()
         @your_contri=Array.new()
         @orgs=@connection.orgs.list.map(&:login)
-        @orgs.each do |org|
-          self.find_repo_of_org(org)
+        results = []
+        @orgs.collect do |org|
+          results << self.find_repo_of_org(org)
         end
+        results
       end
 
       def find_repo_of_org(org)
         @repos_org.push(@connection.repos.all(auto_pagination:true).map(&:name))
         @repos_org.push(@connection.repos.list(auto_pagination: true,user:org).map(&:name))
-        @repos_org.each do |repos|
-          repos.each do |repo|
-            self.find_contributors(org,repo)
+        results = []
+        @repos_org.collect do |repos|
+          repos.collect do |repo|
+            results << self.find_contributors(org,repo)
           end
         end
+        results
       end
 
       def find_contributors(org,repo)
