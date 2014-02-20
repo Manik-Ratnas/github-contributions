f6071499676e9dd4aff39510efd27b588adab39c
Hemali Jain <Hemalijains@gmail.com>
2014-02-19 23:20:30 -0800

    version changed

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -3,13 +3,13 @@ require 'github-contributions'
 
 describe 'github-contributions' do
   before(:all) do
-    my_github_username = 'github-contributions'
-    my_github_password = 'Github143'
-    @connection = Github.new basic_auth: "#{my_github_username}:#{my_github_password}"
+    @my_github_username = 'github-contributions'
+    @my_github_password = 'github143'
+    @connection = Github.new basic_auth: "#{@my_github_username}:#{@my_github_password}"
   end
 
   it 'list your contributions' do
-    list = Github::Contributions::List.new(@connection,my_github_username).list
+    list = Github::Contributions::List.new(@connection,@my_github_username).list
     list.should_not be_nil
   end  
 end
